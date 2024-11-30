182f4405c05633752e9e5ca7613941c06418400e
mastercyrptocoder <l4d2rats@yahoo.com>
2024-11-30 11:22:47 -0800

    dsa

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   const audioRef = useRef(null);
   const hoverAudioRef = useRef(null); // Ref for hover sound
   const [isFadingOut, setIsFadingOut] = useState(false); // To track fade-out
-  const TokenAddress = "TokenUploadedOnLaunch"
+  const TokenAddress = "D3cDjix6ujg7r4fGxKgEFjYLHJpmDugLhj1xJkJspump"
 
   async function fetchFact(month, day, retries = 3, delay = 1000) {
     const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`;
